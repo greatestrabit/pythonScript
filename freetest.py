56f238c2432e351f2700ec0c2d8eb56331ed4109
duguanjun <duguanjun@yunzhangfang.com>
2020-06-12 15:19:00 +0800

    修改测试方法

--- freetest.py
+++ freetest.py
@@ -1,13 +1,14 @@
-from random import randint
-import time
 import subprocess
+import time
+from random import randint
 
 ip_address = ''
 
 while True:
     ip_address = '104.' + str(randint(16, 31)) + '.' + str(randint(0, 255)) + '.' + str(randint(1, 254))
 
-    cmd = 'ping ' + ip_address
+    cmd = 'telnet ' + ip_address+' 443'
+    print(cmd)
 
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
@@ -16,14 +17,11 @@ while True:
         if out == '':
             break
         if out != '':
-            if 'bytes from ' in out:
+            if 'Connected to ' in out:
                 print('找到可用ip: ' + ip_address)
                 print("修改/etc/hosts文件,添加项:\n" + ip_address + " free-ss.site\n访问该网站")
                 exit(0)
-            elif 'PING' in out:
-                print(out)
-            elif 'Unreachable' in out:
-                print(out)
-                break
+            elif 'Trying ' in out:
+                continue
 
     time.sleep(0.5)
